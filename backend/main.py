c2872d8919e97b6f45ffd876a4999ee7b8026d7b
Mohd Ayan Siddiqui <ayansiddiqui1411@gmail.com>
2026-04-13 04:24:49 +0530

    .

--- backend/main.py
+++ backend/main.py
@@ -28,4 +28,6 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
